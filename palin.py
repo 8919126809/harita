e8c64b24120840f3886a1325398b4a97c2cb10f1
8919126809 <42030563+8919126809@users.noreply.github.com>
2018-09-04 16:20:27 +0530

    Update and rename palindrome.py to palin.py

---
+++ palin.py
@@ -0,0 +1,5 @@
+num=int(raw_input("enter the number"))
+if (num==1221):
+      print("num is palindrome")
+else:
+       print(" numb is not palindrome")
--- palindrome.py
+++
@@ -1,5 +0,0 @@
-num=int(raw_input("enter the number"))
-if (num==121):
-      print("number is palindrome")
-else:
-       print(" number is not palindrome")
